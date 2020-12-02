914caa1231d88161b6f4b059e1763793ef68c05e
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-12-02 20:31:24 +0000

    craetinf flatList

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from "./src/screens/HomeScreen";
+import ListScreen from "./src/screens/ListScreen";
 
 const navigator = createStackNavigator(
   {
-    Home: HomeScreen
+    Home: HomeScreen,
+    List: ListScreen
   },
   {
-    initialRouteName: "Home",
+    initialRouteName: "List",
     defaultNavigationOptions: {
       title: "App"
     }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,22 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
-
-const HomeScreen = () => {
-  return<Text style={styles.text}>First timebro</Text>
+const HomeScreen = ()=>{
+  const myName = 'Yusuf';
+  return(
+    <View>
+      <Text style={styles2.text}>MY first React App and gitttttttting styarted with React native</Text>
+      <Text style={styles2.subText}>{myName}</Text>
+    </View>
+  )
 };
 
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 30,
+const styles2 = StyleSheet.create({
+  text:{
+    fontSize: 30
+  },
+  subText:{
+    fontSize: 20,
     color: "red"
   }
 });
