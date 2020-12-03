e2aa3c55228eb3aa82e3697b2120d26f6eff94fc
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-12-03 00:55:06 +0000

    creating vertical friend list

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,23 +3,34 @@ import {View, StyleSheet, Text, FlatList} from 'react-native';
 
 const ListScreen = ()=>{
     const friends=[
-        {name: 'Friend #1'},
-        {name: 'Friend #2'},
-        {name: 'Friend #3'},
-        {name: 'Friend #4'},
-        {name: 'Friend #5'},
-        {name: 'Friend #6'},
-        {name: 'Friend #7'},
-        {name: 'Friend #8'}
+        {name: 'Friend #1', age: '21'},
+        {name: 'Friend #2', age: '22'},
+        {name: 'Friend #3', age: '23'},
+        {name: 'Friend #4', age: '24'},
+        {name: 'Friend #5', age: '25'},
+        {name: 'Friend #6', age: '26'},
+        {name: 'Friend #7', age: '27'},
+        {name: 'Friend #8', age: '28'}
     ]
     return(
         <FlatList
+        horizontal
+        showsHorizontalScrollIndicator = {false}
          keyExtractor={(friend) => friend.name}
          data={friends}
          renderItem={({item}) => {
-             return <Text>{item.name}</Text>
+             return( 
+             <Text style={styles.textStyle}>{item.name} - Age:{item.age}</Text>
+             )
          }} />
     );
 };
+
+const styles = StyleSheet.create({
+    textStyle:{
+        margin: 10,
+        color: 'purple',
+    }
+})
 
 export default ListScreen;
