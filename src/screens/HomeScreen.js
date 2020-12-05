5423c746e42e4ace2a48c2d75b8fe5e6088b18d1
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-12-05 01:29:40 +0000

    adding style to button and creat hr tag

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,23 +1,52 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Button} from 'react-native';
 
-const HomeScreen = ()=>{
-  const myName = 'Yusuf';
+const HomeScreen = (props)=>{
+  const myName = 'Author: Hassan';
   return(
     <View>
-      <Text style={styles2.text}>MY first React App and gitttttttting styarted with React native</Text>
+      <Text style={styles2.text}>De Footbal</Text>
       <Text style={styles2.subText}>{myName}</Text>
+
+      <Text>               
+    _______________________________________________________
+      </Text>
+
+      <TouchableOpacity style={styles2.subText} onPress={()=> props.navigation.navigate('List')}>
+
+        <Text>See Coming Matches</Text>
+        
+       
+      </TouchableOpacity>
+
+      <View
+  style={{
+    borderBottomColor: 'green',
+    borderBottomWidth: 5
+  }}
+/>
+
+      <Button 
+      onPress={()=> props.navigation.navigate('Component')} 
+      title='Go To Predicting Section'>
+
+      </Button>
+
     </View>
   )
 };
 
+
 const styles2 = StyleSheet.create({
   text:{
-    fontSize: 30
+    fontSize: 30,
+    textAlign:"center"
   },
   subText:{
-    fontSize: 20,
-    color: "red"
+    fontSize: 14,
+    color: "red",
+    textAlign: "center",
+    marginBottom:30
   }
 });
 
