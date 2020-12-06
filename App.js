d4b9235780ddfa13e9684b3a921edb694f354b93
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-12-06 02:29:13 +0000

    adding images componenet to app js

--- App.js
+++ App.js
@@ -2,13 +2,17 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from "./src/screens/HomeScreen";
 import ListScreen from "./src/screens/ListScreen";
- import ComponentScreen from "./src/screens/ComponentScreen";
+import ComponentScreen from "./src/screens/ComponentScreen";
+import ImageScreen from "./src/screens/ImagScreen";
+
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     List: ListScreen,
-    Component: ComponentScreen
+    Component: ComponentScreen,
+    Image: ImageScreen
+    
   },
   {
     initialRouteName: "Home",
