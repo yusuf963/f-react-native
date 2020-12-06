cb748e08ba955a20987ee0b3bd140883f1cbd24f
Yusuf <58535314+yusuf963@users.noreply.github.com>
2020-12-06 03:13:10 +0000

    adding score to the image component

--- src/screens/ImagScreen.js
+++ src/screens/ImagScreen.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import { View, StyleSheet} from 'react-native';
-import ImageDetails from '../Components/ImageDetails.js'
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import ImageDetails from "../Components/ImageDetails.js";
 
-const ImageScreen =()=>{
-    return(
-        <View>
-            <ImageDetails tite={'Forest'}/>
-            <ImageDetails title={'Mountain'}/>
-            <ImageDetails title={'Beach'}/>
-            <ImageDetails title={'Space'}/>
-          
-        </View>
-    )
+const ImageScreen = () => {
+  return (
+    <View>
+      <ImageDetails
+        title="Forest"
+        imageSource={require("../../assets/forest.jpg")}
+        score='7/10'
+      />
+      <ImageDetails
+        title="Mountain"
+        imageSource={require("../../assets/mountain.jpg")}
+        score='9/10'
+      />
+      <ImageDetails
+        title="Beach"
+        imageSource={require("../../assets/beach.jpg")}
+        score='7/10'
+      />
+      <ImageDetails
+        title="Space"
+        imageSource={require("../../assets/forest.jpg")}
+        score='10/10'
+      />
+    </View>
+  );
 };
 
-const style= StyleSheet.create({
-    text:{
-        fontSize: 30,
-        color: 'red'
-    }
+const style = StyleSheet.create({
+  text: {
+    fontSize: 30,
+    color: "red",
+  },
 });
 
 export default ImageScreen;
